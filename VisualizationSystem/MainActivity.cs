9e31681e7372d3addfd3321614ffaeb828ab1a24
world666 <world666.kirillov@mail.ru>
2014-04-27 00:22:50 +0300

    kyrylov start

---
+++ VisualizationSystem/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace VisualizationSystem
+{
+    [Activity(Label = "MainActivity", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button logInButton = FindViewById<Button>(Resource.Id.log_in_button);
+            Button signUpButton = FindViewById<Button>(Resource.Id.sign_up_button);
+
+            logInButton.Click += delegate { StartActivity(typeof(AutorizationActivity));};
+            signUpButton.Click += delegate { StartActivity(typeof(RegistrationActivity)); };
+        }
+    }
+}
+
